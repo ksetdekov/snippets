67985195ca422d57d48e52512f1725daa55cca40
Kirill Setdekov <ksetdekov@gmail.com>
2025-03-24 18:42:48 +0300

    added changes

--- sql editing migr matrix/missing_day_types.sql
+++ sql editing migr matrix/missing_day_types.sql
@@ -1,5 +1,5 @@
 with missing_day_types as (
-    SELECT
+    SELECT DISTINCT
         sh.month_begin,
         sh.nm_day_type_day,
         sh.sub_product_type_code,
@@ -9,21 +9,20 @@ with missing_day_types as (
             month_begin,
             nm_day_type_day,
             sub_product_type_code,
-            report_type_day
-            count(*)
-        FROM
-            final_calib
+            report_type_day,
+            count(*) as cnt
+        FROM final_calib
         GROUP BY
             month_begin,
             nm_day_type_day,
             sub_product_type_code,
             report_type_day
     ) fc
-        RIGHT join sh_day_type sh
-            ON sh.nm_day_type_day = fc.nm_day_type_day
-            and sh.report_type_day = fc.report_type_day
-            and sh.month_begin = fc.month_begin
-            and sh.sub_product_type_code = fc.sub_product_type_code
-    WHERE 
-        fc.nm_day_type_day IS NULL
+    RIGHT JOIN sh_day_type sh
+        ON sh.nm_day_type_day = fc.nm_day_type_day
+       AND sh.report_type_day = fc.report_type_day
+       AND sh.month_begin = fc.month_begin
+       AND sh.sub_product_type_code = fc.sub_product_type_code
+    WHERE fc.nm_day_type_day IS NULL
+)
 SELECT * FROM missing_day_types;
